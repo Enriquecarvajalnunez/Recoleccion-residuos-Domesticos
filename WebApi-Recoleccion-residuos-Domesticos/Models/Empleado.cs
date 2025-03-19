470bb671ae7c31e45fd24ce6e6f90bde958fd24a
Enriquecarvajalnunez <63138636+Enriquecarvajalnunez@users.noreply.github.com>
2025-03-18 20:52:11 -0500

    se realiza proyecto api-rest

---
+++ WebApi-Recoleccion-residuos-Domesticos/Models/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi_Recoleccion_residuos_Domesticos.Models;
+
+public partial class Empleado
+{
+    public int IdEmpleado { get; set; }
+
+    public string Identificacion { get; set; } = null!;
+
+    public string Nombres { get; set; } = null!;
+
+    public string Apellidos { get; set; } = null!;
+
+    public string? Direccion { get; set; }
+
+    public string? Telefono { get; set; }
+}
--- WebApi-Recoleccion-residuos-Domesticos/Program.cs
+++ WebApi-Recoleccion-residuos-Domesticos/Program.cs
@@ -1,3 +1,7 @@
+
+using Microsoft.EntityFrameworkCore;
+using WebApi_Recoleccion_residuos_Domesticos.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Asignamos la cadena de conexión
+builder.Services.AddDbContext<RecoleccionResiduosContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
+});
 
 var app = builder.Build();
 
